6a251288af2df1ccbf8952a469dc12efe5595a7f
Cody Condon <codycondon50@gmail.com>
2016-04-02 09:33:14 -0400

    working with functions and strings

--- Code/app.js
+++ Code/app.js
@@ -1,12 +1,22 @@
-//The scope service
+//THE SCOPE SER
+// var myApp = angular.module('myApp', []);
+// myApp.controller('mainController', function($scope) {
+//   $scope.name = "cody";
+//   $scope.skill = "programming";
+//   $scope.getName = function() {
+//     return "john doe";
+//   }
+//
+//   console.log($scope)
+//
+// });
+
+//FUNCTIONS AND STRINGS
 var myApp = angular.module('myApp', []);
 myApp.controller('mainController', function($scope) {
-  $scope.name = "cody";
-  $scope.skill = "programming";
-  $scope.getName = function() {
-    return "john doe";
-  }
-
-  console.log($scope)
-
 });
+// In angular you can pass functions as strings, which later on, peices can be parsed out
+var searchPeople = function searchPeople(firstName, lastName, height,age,occupation) {
+  return 'Jane Doe';
+}
+console.log(searchPeople);
