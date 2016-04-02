c91761b2c1956403d6f4eaeef47cb0bca650333e
Cody Condon <codycondon50@gmail.com>
2016-04-02 12:49:34 -0400

    working with watchers and the digest loop

--- Angular-Part-Two/app.js
+++ Angular-Part-Two/app.js
@@ -1,6 +1,24 @@
+//WATCHERS AND THE DIGEST LOOP
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', function($scope) {
-    
-    
+myApp.controller('mainController', ['$scope', '$filter','$timeout', function($scope,$filter,$timeout) {
+  $scope.handle = '';
+
+  $scope.lowercasehandle = function() {
+    return $filter('lowercase')($scope.handle);
+  };
+
+  $scope.$watch('handle', function(newValue, oldValue){
+    console.info('Changed!');
+    console.log('old:' + oldValue);
+    console.log('new:' + newValue);
+
+  });
+  $timeout(function() {
+    $scope.$apply(function(){
+    $scope.handle = 'newtwitterhandle';
+    console.log('scope changed');
+  });
+  }, 3000);
+
 }]);
