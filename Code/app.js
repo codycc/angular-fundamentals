cbf23165fd5d6b969566c7ded795e2d6d545ac5f
Cody Condon <codycondon50@gmail.com>
2016-04-02 09:49:29 -0400

    going over dependency injection

--- Code/app.js
+++ Code/app.js
@@ -12,11 +12,23 @@
 // });
 
 //FUNCTIONS AND STRINGS
-var myApp = angular.module('myApp', []);
-myApp.controller('mainController', function($scope) {
-});
-// In angular you can pass functions as strings, which later on, peices can be parsed out
-var searchPeople = function searchPeople(firstName, lastName, height,age,occupation) {
-  return 'Jane Doe';
-}
-console.log(searchPeople);
+// var myApp = angular.module('myApp', []);
+// myApp.controller('mainController', function($scope) {
+// });
+// // In angular you can pass functions as strings, which later on, peices can be parsed out
+// var searchPeople = function searchPeople(firstName, lastName, height,age,occupation) {
+//   return 'Jane Doe';
+// }
+// console.log(searchPeople);
+
+//DEPENDENCY INJECTION
+// var myApp = angular.module('myApp', []);
+// myApp.controller('mainController', function($scope) {
+//   console.log($scope);
+// });
+// // In angular you can pass functions as strings, which later on, peices can be parsed out
+// var searchPeople = function searchPeople(firstName, $scope, height,age,occupation) {
+//   return 'Jane Doe';
+// }
+// //Parses the string and creates an array out of the parameters
+// console.log(angular.injector().annotate(searchPeople));
