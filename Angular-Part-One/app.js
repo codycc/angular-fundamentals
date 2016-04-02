c691b083755f56aa5bff544a534c9830d97a75ec
Cody Condon <codycondon50@gmail.com>
2016-04-02 11:52:20 -0400

    Add part two foler

--- Angular-Part-One/app.js
+++ Angular-Part-One/app.js
@@ -98,7 +98,7 @@
 // }]);
 
 
-//DIRECTIVE AND TWO WAY DATA BINDING
+// DIRECTIVE AND TWO WAY DATA BINDING
 var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
