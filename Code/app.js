ba675df16eff14ecc5fb7c85a17d0e69b7da5a3d
Cody Condon <codycondon50@gmail.com>
2016-04-02 10:48:42 -0400

    working with minification and ngResource

--- Code/app.js
+++ Code/app.js
@@ -11,6 +11,7 @@
 //
 // });
 
+
 //FUNCTIONS AND STRINGS
 // var myApp = angular.module('myApp', []);
 // myApp.controller('mainController', function($scope) {
@@ -20,6 +21,7 @@
 //   return 'Jane Doe';
 // }
 // console.log(searchPeople);
+
 
 //DEPENDENCY INJECTION
 // var myApp = angular.module('myApp', []);
@@ -32,3 +34,51 @@
 // }
 // //Parses the string and creates an array out of the parameters
 // console.log(angular.injector().annotate(searchPeople));
+
+
+//GETTING OTHER SERVICES
+// var myApp = angular.module('myApp', []);
+// myApp.controller('mainController', function($log,$scope, $filter) {
+//   //Log service enchances console.log
+//   $log.log("Hello");
+//   $log.info("Here is some information");
+//   $log.warn("Warning!");
+//   $log.debug('Some debug information while writing my code');
+// });
+
+
+// $SCOPE $FILTER AND $LOG
+// var myApp = angular.module('myApp', []);
+// myApp.controller('mainController', function($log,$scope,$filter) {
+//   $scope.name = "cody";
+//   //Using $filter $scope and $log together
+//   $scope.formattedname = $filter('uppercase')($scope.name);
+//
+//   $log.info($scope.name); // cody
+//   $log.info($scope.formattedname) // CODY
+// });
+
+//Using ngResource
+// var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
+// myApp.controller('mainController', function($log,$scope,$filter,$resource) {
+//
+//   console.log($resource)
+//
+// });
+
+// NOTETOSELF : CHECK OUT NGMODULES, CODE CAN BE GRABBED, REFERENCED
+// AS DEPENDENCY AND USED
+
+// //DEALING WITH MINIFICATION
+// var myApp = angular.module('myApp', []);
+// // the order matters, angular looks at first two elements of array and matches them with
+// // the order of the parameters
+// myApp.controller('mainController',['$scope', '$log', function($scope, $log) {
+//
+//   $log.info($scope);
+//
+// }]);
+
+//Minifier will never touch whats inside a string
+//MINIFIED VERSION
+// myApp.controller("mainController",["$scope","$log",function(o,n){o.info(n)}]);
