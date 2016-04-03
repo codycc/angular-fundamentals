91e9f0dcd7a8444ce474e36c3221dfcc60da294e
Cody Condon <codycondon50@gmail.com>
2016-04-03 09:53:24 -0400

    single page application working with routing templates and controllers

--- Angular-Part-Two/app.js
+++ Angular-Part-Two/app.js
@@ -88,3 +88,29 @@
 //
 //   $scope.name = 'Second';
 // }]);
+
+//ROUTING, TEMPLATES, AND CONTROLLERS
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider){
+  $routeProvider
+  .when('/', {
+    templateUrl: 'pages/main.html',
+    controller: 'mainController'
+  })
+  .when('/second', {
+    templateUrl: 'pages/second.html',
+    controller: 'secondController'
+  })
+
+});
+
+myApp.controller('mainController', ['$scope', '$location', '$log', function($scope, $location, $log){
+  $scope.name = 'Main';
+
+}]);
+
+myApp.controller('secondController', ['$scope', '$location', '$log', function($scope, $location, $log){
+  $scope.name = "Second";
+
+}]);
